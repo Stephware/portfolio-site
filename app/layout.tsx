8cada0a104a4401fc20404907899758bf9951b5d
Stephware <pinacate.stephen@gmail.com>
2026-08-30 11:30:15 +0800

    style: load mobile hero ordering styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./theme.css";
 import "./profile.css";
+import "./mobile.css";
 
 const geist = Geist({
   variable: "--font-geist-sans",
